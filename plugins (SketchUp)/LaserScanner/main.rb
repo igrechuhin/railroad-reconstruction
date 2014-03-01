6172411e4883d39dca87b5cbf354a2c45d881f68
Abramenko A.A. <mr.abramenko@gmail.com>
2014-03-01 19:20:20 +0400

    Fix
    - Добавлена возможность задавать угол между осью OY и  плоскостью
    сканирования

--- plugins (SketchUp)/LaserScanner/main.rb
+++ plugins (SketchUp)/LaserScanner/main.rb
@@ -1,8 +1,6 @@
 # LaserScanner
 
 module LaserScanner
-  @debag=false
-
   def self.preferences
  
 
@@ -10,15 +8,16 @@ module LaserScanner
     bbox = @model.bounds
     @length = bbox.corner(7).y.to_f.to_m # (7 - right back top corner of the bounding box)
     
-    prompts = [ "The length of the path trains (m): ","The height of the attachment point of the camera (m): ", "d(fi)  (in degrees)","d(y)  (in meters)"]
-    defaults = ["#{@length}","2","1","0.1"]
-    list = ["","","",""]
+    prompts = [ "The length of the path trains (m): ","The height of the attachment point of the camera (m): ", "d(fi)  (in degrees)","d(y)  (in meters)","Angle of rotation of the plane of laser scanning (in degrees): "]
+    defaults = ["#{@length}","2","1","0.1","90"]
+    list = ["","","","",""]
     input = UI.inputbox prompts, defaults, list, "Railway Laser Scanner"
     return unless input
     @length = input[0].to_f.m.to_f
     @laser_height = input[1].to_f.m.to_f
     @d_fi = input[2].to_f.degrees
     @d_y = input[3].to_f.m.to_f
+    @angle = input[4].to_f.degrees
 
   
     title = @model.title
@@ -28,13 +27,6 @@ module LaserScanner
   def self.scan
     return unless @path_to_save_to
     puts "Cartesian coordinate system (X,Y,Z)"
-    p "@laser_height #{@laser_height}" if @debag
-    p "@length #{@length}" if @debag
-    p "@d_y #{@d_y}" if @debag
-    p "(@length/@d_y).floor #{(@length/@d_y).floor}" if @debag
-    p "@d_fi #{@d_fi}" if @debag
-    p "(Math::PI/@d_fi).floor #{(Math::PI/@d_fi).floor}" if @debag
-    puts ">> Scanning started. #{((@length/@d_y).floor+1)*((Math::PI/@d_fi).floor+1)} points. It may take a few minutes..."
     f = File.new( @path_to_save_to,  "w+")
     l_position = Geom::Point3d.new
     l_vector = Geom::Vector3d.new
@@ -42,23 +34,47 @@ module LaserScanner
     l_position.x= 0
     l_position.z= @laser_height
     l_vector.y= 0
-    for i in 0..(@length/@d_y).floor 
+    if @angle == 90.degrees
+      puts ">> Scanning started. #{((@length/@d_y).floor+1)*((2*Math::PI/@d_fi).floor+1)} points. It may take a few minutes..."
+      for i in 0..(@length/@d_y).floor 
       l_position.y= i*@d_y
-      for j in 0..(Math::PI/@d_fi).floor
+      for j in 0..(2*Math::PI/@d_fi).floor
         fi = (-1.0/2)*Math::PI+j*@d_fi
         l_vector.x= Math::cos(fi)
         l_vector.z= Math::sin(fi)
-        p "_____________#{i}/#{j}" if @debag
-        p l_position.to_s if @debag
-        p l_vector.to_s if @debag
+        
+        l_vector.y= Math::sin(@angle.degrees)
         item = @model.raytest([l_position, l_vector])
         unless item.nil? 
           f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
         end#unless
-     end#for
-    end#for
+      end#for
+      end#for
+    else
+      puts ">> Scanning started. #{2*((@length/@d_y).floor+1)*((2*Math::PI/@d_fi).floor+1)} points. It may take a few minutes..."
+      for i in 0..(@length/@d_y).floor 
+      l_position.y= i*@d_y
+      for j in 0..(2*Math::PI/@d_fi).floor
+        fi = (-1.0/2)*Math::PI+j*@d_fi
+        l_vector.x= Math::cos(fi)
+        l_vector.z= Math::sin(fi)
+        
+        l_vector.y= Math::sin(@angle)
+        item = @model.raytest([l_position, l_vector])
+        unless item.nil? 
+          f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
+        end#unless
+        l_vector.y= Math::sin(-1*@angle)
+        item = @model.raytest([l_position, l_vector])
+        unless item.nil? 
+          f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
+        end#unless
+      end#for
+      end#for
+  end#if
    f.close
    puts ">> Scan completed!"
+   @path_to_save_to = nil;
  
   end#def
 
