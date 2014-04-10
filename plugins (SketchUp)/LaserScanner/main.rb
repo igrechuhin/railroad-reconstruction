761624e85a20d1b05e5326159f1cf517fb568e0c
Abramenko Alexander <mr.abramenko@gmail.com>
2014-04-10 16:15:34 +0400

    Дабовлены случайные колебания лазера
    Случайные сдвиги лазерного луча в доль всех осей на случайные величины,
    которые имеют распределение Гаусса с математическим ожиданием равным 0.
    Среднеквадратическое отклонение задается пользователем.

--- plugins (SketchUp)/LaserScanner/main.rb
+++ plugins (SketchUp)/LaserScanner/main.rb
@@ -7,10 +7,9 @@ module LaserScanner
     @model = Sketchup.active_model
     bbox = @model.bounds
     @length = bbox.corner(7).y.to_f.to_m # (7 - right back top corner of the bounding box)
-    
-    prompts = [ "The length of the path trains (m): ","The height of the attachment point of the camera (m): ", "d(fi)  (in degrees)","d(y)  (in meters)","Angle of rotation of the plane of laser scanning (in degrees): "]
-    defaults = ["#{@length}","2","1","0.1","90"]
-    list = ["","","","",""]
+    prompts = [ "Длина пути пройденного поездом (в метрах): ","Высота точки подвеса камеры (в метрах): ", "Шаг d(fi) по углу вращения (в градусах)","Шаг поступательного движения поезда d(y)  (в метр)","Угол между плоскостью вращения лазера и осью ОУ (in degrees): ","Среднеквадрати́чное отклоне́ние лазерного луча (в градусах): "]
+    defaults = ["#{@length}","2","1","0.1","90", "0.01"]
+    list = ["","","","","",""]
     input = UI.inputbox prompts, defaults, list, "Railway Laser Scanner"
     return unless input
     @length = input[0].to_f.m.to_f
@@ -18,8 +17,7 @@ module LaserScanner
     @d_fi = input[2].to_f.degrees
     @d_y = input[3].to_f.m.to_f
     @angle = input[4].to_f.degrees
-
-  
+    @SD = input[5].to_f.degrees
     title = @model.title
     @path_to_save_to = UI.savepanel "Save scan data", "c:\\", "#{title}.csv"
   end#def
@@ -40,10 +38,9 @@ module LaserScanner
       l_position.y= i*@d_y
       for j in 0..(2*Math::PI/@d_fi).floor
         fi = (-1.0/2)*Math::PI+j*@d_fi
-        l_vector.x= Math::cos(fi)
-        l_vector.z= Math::sin(fi)
-        
-        l_vector.y= Math::sin(@angle.degrees)
+        l_vector.x= Math.cos(fi + self.rand_normal)
+        l_vector.z= Math.sin(fi + self.rand_normal)
+        l_vector.y= Math.cos(@angle + self.rand_normal)
         item = @model.raytest([l_position, l_vector])
         unless item.nil? 
           f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
@@ -56,26 +53,37 @@ module LaserScanner
       l_position.y= i*@d_y
       for j in 0..(2*Math::PI/@d_fi).floor
         fi = (-1.0/2)*Math::PI+j*@d_fi
-        l_vector.x= Math::cos(fi)
-        l_vector.z= Math::sin(fi)
-        
-        l_vector.y= Math::sin(@angle)
+        l_vector.x= Math.cos(fi + self.rand_normal)
+        l_vector.z= Math.sin(fi + self.rand_normal)
+        l_vector.y= Math.cos(@angle + self.rand_normal)
         item = @model.raytest([l_position, l_vector])
         unless item.nil? 
           f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
         end#unless
-        l_vector.y= Math::sin(-1*@angle)
+        l_vector.y= -1*Math.cos(@angle + self.rand_normal)
         item = @model.raytest([l_position, l_vector])
         unless item.nil? 
           f.write("#{item[0].x.to_f.to_m},#{item[0].y.to_f.to_m},#{item[0].z.to_f.to_m}\n")
         end#unless
       end#for
       end#for
-  end#if
+    end#if
    f.close
    puts ">> Scan completed!"
    @path_to_save_to = nil;
- 
+  end#def
+
+  def self.rand_normal
+    r = Random.new
+    x = r.rand * (-1)** r.rand(100)
+    y = r.rand * (-1)** r.rand(100)
+    while x**2+y**2 > 1 do
+      x = r.rand * (-1)** r.rand(100)
+      y = r.rand * (-1)** r.rand(100)
+    end
+    s = x**2+y**2
+    z = x*Math.sqrt(-2*Math.log(s)/s)
+    return Math.sqrt(@SD)*z
   end#def
 
   def self.start
@@ -86,11 +94,9 @@ module LaserScanner
       Sketchup.active_model.commit_operation
   end#def
 
-
 #Menus and toolbars
 file = File.basename(__FILE__)
   unless file_loaded?(file)
     UI.menu("Plugins").add_item("Railway Laser Scanner") {self.start}
   end#unless
-
 end#module
